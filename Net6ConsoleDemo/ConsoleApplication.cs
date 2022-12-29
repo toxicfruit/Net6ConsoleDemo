eab21841326ed1c882bbd123658ff31663e2401d
Oleg Zamsky <Oleg.Zamsky@vu.com>
2022-12-29 11:14:27 -0600

    Add project files.

---
+++ Net6ConsoleDemo/ConsoleApplication.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Net6ConsoleDemo
+{
+    public class ConsoleApplication
+    {
+        private readonly ILogger<ConsoleApplication> logger;
+
+        public ConsoleApplication(ILogger<ConsoleApplication> logger)
+        {
+            this.logger = logger;
+        }
+        public Task RunAsync()
+        {
+            logger.LogInformation("Hello World!");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Net6ConsoleDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Net6ConsoleDemo;
+
+var host = Host
+    .CreateDefaultBuilder(args)
+    .ConfigureLogging((context, logging) =>
+    {
+        logging
+            .ClearProviders()
+            .AddSimpleConsole(options => options.TimestampFormat = "[HH:mm:ss.fff MM/dd/yyyy] ");
+    })
+    .ConfigureAppConfiguration((context, configure) =>
+    {
+        configure.Sources.Clear();
+        configure
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+            //.AddUserSecrets<Program>()
+            .AddEnvironmentVariables()
+            .AddCommandLine(args);
+    })
+    .ConfigureServices((context, services) =>
+    {
+        var configuration = context.Configuration;
+        services
+            .AddSingleton<ConsoleApplication>();
+    })
+    .Build();
+
+var application = host.Services.GetRequiredService<ConsoleApplication>();
+await application.RunAsync();
+await Task.Delay(10);
